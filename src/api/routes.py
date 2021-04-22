46345282d325a3943f862ee963e4146d59dfaa9e
chip-student <omoram28@gmail.com>
2021-04-22 01:48:21 +0000

    endpoint register

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,35 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    
+    email = request.json.get("email", None)
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    # validar que contengan informacion 
+    if email is None or username is None or password is None:
+        raise APIException('Correo, nombre de usuario o contraseña no encontrados', status_code=400)
+    # valida que el email, username y password no estn en vacios
+    if email == "" or username == "" or password == "":
+        raise APIException('Correo, nombre de usuario o contraseña estan vacios', status_code=400)
+    # valida si el email ya existe
+    emailrepetido = User.query.filter_by(email=email).first()
+    emailrepetido = list(map(lambda x: x.serialize(), emailrepetido))
+    if emailrepetido != []:
+        raise APIException('El correo ya existe', status_code=400)
+    # valida si el usuario ya existe
+    userrepetido = User.query.filter_by(username=username).first()
+    userrepetido = list(map(lambda x: x.serialize(), userrepetido))
+    if userrepetido != []:
+        raise APIException('El usuario ya se encuentra registrado', status_code=400)
+    
+    # registro del nuevo usuario
+    newUser = User(username=username, email=email,
+    password=password)
+    db.session.add(newUser)
+    db.session.commit()
+
+    return jsonify('OK'), 200
