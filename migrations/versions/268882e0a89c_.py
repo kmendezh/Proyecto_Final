cf2c9458d4f3f8dc0263f9033b4b224396153fd8
chip-student <omoram28@gmail.com>
2021-04-23 17:50:26 +0000

    cambios login con token jwt

--- migrations/versions/268882e0a89c_.py
+++ migrations/versions/268882e0a89c_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 35d8cd62328c
+Revision ID: 268882e0a89c
 Revises: 
-Create Date: 2021-04-22 01:25:08.443573
+Create Date: 2021-04-23 16:36:16.068694
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '35d8cd62328c'
+revision = '268882e0a89c'
 down_revision = None
 branch_labels = None
 depends_on = None
