d29b4795cfa29fcf12d4189f359211b40819758e
YeiDiaz <yei.diaz93@gmail.com>
2021-04-27 00:28:20 +0000

    nuevos

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,9 +3,19 @@ import "../../styles/footer.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div className="col item social">
+			<a href="https://www.facebook.com/PuraVidaPassport/" target="_blank" rel="noopener noreferrer">
+				<i className="fab fa-facebook-f" />
+			</a>
+			<a href="#">
+				<i className="fab fa-twitter" />
+			</a>
+			<a href="#">
+				<i className="fab fa-snapchat-ghost" />
+			</a>
+			<a href="#">
+				<i className="fab fa-instagram" />
+			</a>
+		</div>
 	</footer>
 );
