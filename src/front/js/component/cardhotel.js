7f890bb300d02c6cc91de9ee9c07a44b0ee1f8d8
chip-student <omoram28@gmail.com>
2021-05-01 03:05:50 +0000

    Cambios en vista hoteles y restaurantes

--- src/front/js/component/cardhotel.js
+++ src/front/js/component/cardhotel.js
@@ -15,14 +15,9 @@ const CardHotel = props => {
 						</div>
 						<div className="d-flex flex-column p-0">
 							<p>{props.descripcion}</p>
-							<p className="p-0">Descuentos: {props.descuento} %</p>
-						</div>
-						<div className="d-flex justify-content-start">
-							<Link to={""}>
-								<button type="button" className="btn btn-outline-primary">
-									Ver mas
-								</button>
-							</Link>
+							<p className="p-0">
+								Descuentos desde {props.descuento1} % hasta {props.descuento2} %{" "}
+							</p>
 						</div>
 					</div>
 				</div>
@@ -35,7 +30,8 @@ CardHotel.propTypes = {
 	link: PropTypes.string,
 	title: PropTypes.string,
 	descripcion: PropTypes.string,
-	descuento: PropTypes.number
+	descuento1: PropTypes.number,
+	descuento2: PropTypes.number
 };
 
 export default CardHotel;
